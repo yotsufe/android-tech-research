b75afb6f441bc7239451271f95a00da267aef4fa
yotsufe <yotsufe@gmail.com>
2020-12-25 21:29:22 +0900

    network接続検知 ・不要箇所削除

--- app/src/main/java/com/yotsufe/techresearch/NetworkTestActivity.kt
+++ app/src/main/java/com/yotsufe/techresearch/NetworkTestActivity.kt
@@ -12,7 +12,6 @@ import com.yotsufe.techresearch.databinding.ActivityNetworkTestBinding
 class NetworkTestActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNetworkTestBinding
-    private var isOnline = false
     private val connectivityManager by lazy {
         applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     }
@@ -20,10 +19,6 @@ class NetworkTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_network_test)
-
-        val currentNetwork = connectivityManager.activeNetwork
-        val caps = connectivityManager.getNetworkCapabilities(currentNetwork)
-        Log.d("###", "")
     }
 
     override fun onResume() {
@@ -32,29 +27,21 @@ class NetworkTestActivity : AppCompatActivity() {
             connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network : Network) {
                     Log.e("###", "onAvailable")
-                    Log.e("###", "The default network is now: " + network)
                 }
 
                 override fun onLost(network : Network) {
                     Log.e("###", "onLost")
-                    Log.e("###", "The application no longer has a default network. The last default network was " + network)
                 }
 
                 override fun onCapabilitiesChanged(network : Network, networkCapabilities : NetworkCapabilities) {
                     Log.e("###", "onCapabilitiesChanged")
-                    Log.e("###", "The default network changed capabilities: " + networkCapabilities)
                 }
 
                 override fun onLinkPropertiesChanged(network : Network, linkProperties : LinkProperties) {
                     Log.e("###", "onLinkPropertiesChanged")
-                    Log.e("###", "The default network changed link properties: " + linkProperties)
                 }
             })
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-//        connectivityManager.unregisterNetworkCallback()
-    }
 }
