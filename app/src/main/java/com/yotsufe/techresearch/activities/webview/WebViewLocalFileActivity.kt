732fee7c255bc571aacc8c9473766631dc68393d
yotsufe <yotsufe@gmail.com>
2021-06-03 00:21:54 +0900

    •WebViewにファイル選択欄追加

--- app/src/main/java/com/yotsufe/techresearch/activities/webview/WebViewLocalFileActivity.kt
+++ app/src/main/java/com/yotsufe/techresearch/activities/webview/WebViewLocalFileActivity.kt
@@ -1,15 +1,16 @@
 package com.yotsufe.techresearch.activities.webview
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.JavascriptInterface
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.yotsufe.techresearch.R
 import com.yotsufe.techresearch.databinding.ActivityWebViewTestBinding
+import java.lang.Exception
 
 class WebViewLocalFileActivity : AppCompatActivity() {
 
@@ -21,9 +22,22 @@ class WebViewLocalFileActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityWebViewTestBinding
-
     private var email: String = ""
     private var message: String = ""
+    private var uploadMessage: ValueCallback<Array<Uri>>? = null
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode != Activity.RESULT_OK) {
+                return@registerForActivityResult
+            }
+            if (result.data != null) {
+                uploadMessage?.onReceiveValue(
+                    WebChromeClient.FileChooserParams.parseResult(result.resultCode, result.data)
+                )
+                uploadMessage = null
+            }
+    }
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,6 +58,30 @@ class WebViewLocalFileActivity : AppCompatActivity() {
                     super.onPageFinished(view, url)
                     view?.loadUrl( "javascript:setValue('message_body', '$message')")
                     view?.loadUrl( "javascript:setValue('email_body', '$email')")
+                }
+            }
+            it.webChromeClient = object : WebChromeClient() {
+                override fun onShowFileChooser(
+                    webView: WebView?,
+                    filePathCallback: ValueCallback<Array<Uri>>?,
+                    fileChooserParams: FileChooserParams?
+                ): Boolean {
+                    if (uploadMessage != null) {
+                        uploadMessage?.onReceiveValue(null)
+                        uploadMessage = null
+                    }
+
+                    uploadMessage = filePathCallback
+
+                    try {
+                        fileChooserParams?.createIntent().run {
+                            startForResult.launch(this)
+                        }
+                    } catch (e: Exception) {
+                        uploadMessage = null
+                        return false
+                    }
+                    return true
                 }
             }
             it.settings.javaScriptEnabled = true
